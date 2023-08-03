e64f22a8a8a76a2bae7adb5a9b3e3664282f72ca
Georg Veichtlbauer <georg@vware.at>
2023-08-03 08:00:24 +0200

    LatinIME: Add option to always show the emoji key
    Change-Id: I5951524caba8413e9a0244ddc43869b4d1c97007

--- java/src/com/android/inputmethod/keyboard/KeyboardLayoutSet.java
+++ java/src/com/android/inputmethod/keyboard/KeyboardLayoutSet.java
@@ -120,6 +120,7 @@ public final class KeyboardLayoutSet {
         boolean mNoSettingsKey;
         boolean mNumberRowEnabled;
         boolean mLanguageSwitchKeyEnabled;
+        boolean mEmojiKeyEnabled;
         RichInputMethodSubtype mSubtype;
         boolean mIsSpellChecker;
         int mKeyboardWidth;
@@ -330,6 +331,11 @@ public final class KeyboardLayoutSet {
 
         public Builder setLanguageSwitchKeyEnabled(final boolean enabled) {
             mParams.mLanguageSwitchKeyEnabled = enabled;
+            return this;
+        }
+
+        public Builder setEmojiKeyEnabled(final boolean enabled) {
+            mParams.mEmojiKeyEnabled = enabled;
             return this;
         }
 
