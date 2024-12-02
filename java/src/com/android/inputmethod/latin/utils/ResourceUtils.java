d3e55730cb40eb9658022107205220072c9202c7
Michael W <baddaemon87@gmail.com>
2024-12-02 19:37:29 +0100

    LatinIME: Fix landscape mode
    Since the insets are not taken into account in landscape mode (likely
    also in portrait, but who has a cutout there...), we have to fix that
    ourselves.
    According to
    https://developer.android.com/reference/android/content/res/Resources#getDisplayMetrics()
    it's best practise to use WindowManager#getCurrentWindowMetrics() which
    in turn states: "The WindowInsets are not deducted from the bounds"
    So - we do it ourselves!
    
    Change-Id: I7aaab08a78568c9348b1a7efcbb60cf6f7f8f4da

--- java/src/com/android/inputmethod/latin/utils/ResourceUtils.java
+++ java/src/com/android/inputmethod/latin/utils/ResourceUtils.java
@@ -16,13 +16,18 @@
 
 package com.android.inputmethod.latin.utils;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Insets;
 import android.os.Build;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+import android.view.WindowMetrics;
 
 import com.android.inputmethod.annotations.UsedForTesting;
 import com.android.inputmethod.latin.R;
@@ -182,9 +187,14 @@ public final class ResourceUtils {
         return matchedAll;
     }
 
-    public static int getDefaultKeyboardWidth(final Resources res) {
-        final DisplayMetrics dm = res.getDisplayMetrics();
-        return dm.widthPixels;
+    public static int getDefaultKeyboardWidth(final Context context) {
+        WindowManager wm = context.getSystemService(WindowManager.class);
+        WindowMetrics metrics = wm.getCurrentWindowMetrics();
+
+        Insets insets = metrics.getWindowInsets().getInsetsIgnoringVisibility(
+                WindowInsets.Type.systemBars() | WindowInsets.Type.displayCutout());
+
+        return metrics.getBounds().width() - insets.left - insets.right;
     }
 
     public static int getKeyboardHeight(final Resources res, final SettingsValues settingsValues) {
