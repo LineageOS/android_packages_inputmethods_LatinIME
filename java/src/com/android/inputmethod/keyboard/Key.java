1c1d26c78c72ead05f70efa21ce3efd50b764617
Patrick Gaskin <patrick@pgaskin.net>
2026-03-19 06:35:23 -0400

    LatinIME: Support overriding the uppercase version of a key on a per-key basis
    Change-Id: I6e5b4121a6b5ea2c552cf8716fe64f384a40f8f8

--- java/src/com/android/inputmethod/keyboard/Key.java
+++ java/src/com/android/inputmethod/keyboard/Key.java
@@ -255,6 +255,8 @@ public class Key implements Comparable<Key> {
     public Key(@Nullable final String keySpec, @Nonnull final TypedArray keyAttr,
             @Nonnull final KeyStyle style, @Nonnull final KeyboardParams params,
             @Nonnull final KeyboardRow row) {
+        final String keySpecUpper = style.getString(keyAttr, R.styleable.Keyboard_Key_keySpecUpper);
+
         mHorizontalGap = isSpacer() ? 0 : params.mHorizontalGap;
         mVerticalGap = params.mVerticalGap;
 
@@ -340,7 +342,8 @@ public class Key implements Comparable<Key> {
         final int disabledIconId = KeySpecParser.getIconId(style.getString(keyAttr,
                 R.styleable.Keyboard_Key_keyIconDisabled));
 
-        final int code = KeySpecParser.getCode(keySpec);
+        final int code = KeySpecParser.getCode(
+                (needsToUpcase && keySpecUpper != null) ? keySpecUpper : keySpec);
         if ((mLabelFlags & LABEL_FLAGS_FROM_CUSTOM_ACTION_LABEL) != 0) {
             mLabel = params.mId.mCustomActionLabel;
         } else if (code >= Character.MIN_SUPPLEMENTARY_CODE_POINT) {
@@ -351,7 +354,9 @@ public class Key implements Comparable<Key> {
         } else {
             final String label = KeySpecParser.getLabel(keySpec);
             mLabel = needsToUpcase
-                    ? StringUtils.toTitleCaseOfKeyLabel(label, localeForUpcasing)
+                    ? keySpecUpper != null
+                        ? KeySpecParser.getLabel(keySpecUpper)
+                        : StringUtils.toTitleCaseOfKeyLabel(label, localeForUpcasing)
                     : label;
         }
         if ((mLabelFlags & LABEL_FLAGS_DISABLE_HINT_LABEL) != 0) {
@@ -365,7 +370,9 @@ public class Key implements Comparable<Key> {
         }
         String outputText = KeySpecParser.getOutputText(keySpec);
         if (needsToUpcase) {
-            outputText = StringUtils.toTitleCaseOfKeyLabel(outputText, localeForUpcasing);
+            outputText = keySpecUpper != null
+                    ? KeySpecParser.getOutputText(keySpecUpper)
+                    : StringUtils.toTitleCaseOfKeyLabel(outputText, localeForUpcasing);
         }
         // Choose the first letter of the label as primary code if not specified.
         if (code == CODE_UNSPECIFIED && TextUtils.isEmpty(outputText)
@@ -392,7 +399,9 @@ public class Key implements Comparable<Key> {
                 mCode = CODE_OUTPUT_TEXT;
             }
         } else {
-            mCode = needsToUpcase ? StringUtils.toTitleCaseOfKeyCode(code, localeForUpcasing)
+            // If keySpecUpper is not null, we've already upcased the code if required
+            mCode = (needsToUpcase && keySpecUpper == null)
+                    ? StringUtils.toTitleCaseOfKeyCode(code, localeForUpcasing)
                     : code;
         }
         final int altCodeInAttr = KeySpecParser.parseCode(
