87f7424213e120fd91fbcda09ffc36970a113f6a
Patrick Gaskin <patrick@pgaskin.net>
2026-03-19 05:49:15 -0400

    LatinIME: Support special characters on number row
    Change-Id: I001f557d392357f0b1669966c6741f308e75aa04

--- common/src/com/android/inputmethod/latin/common/StringUtils.java
+++ common/src/com/android/inputmethod/latin/common/StringUtils.java
@@ -603,6 +603,18 @@ public final class StringUtils {
         if (label == null) {
             return label;
         }
+        switch (label) { // TODO: find a better place for this (possibly in the xml) so it only applies to the optional number row and can handle different layouts
+            case "1": return "!";
+            case "2": return "@";
+            case "3": return "#";
+            case "4": return "$";
+            case "5": return "%";
+            case "6": return "^";
+            case "7": return "&";
+            case "8": return "*";
+            case "9": return "(";
+            case "0": return ")";
+        }
         return label.toUpperCase(getLocaleUsedForToTitleCase(locale));
     }
 
